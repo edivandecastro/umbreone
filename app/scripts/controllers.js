ab39724abaf4952656e5d57e9403b44f27e06140
Edivan de Castro <edivandecastro@gmail.com>
2016-06-26 12:29:43 -0300

    Implemented delete for brand

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,13 +7,22 @@ angular.module('UmbreoneApp.controllers', [])
         var self = this;
 
         self.brands = Brand.query();
-        console.log(self.brands);
 
-        self.save = function () {
+        self.save = function() {
             self.obj.brand.enterprise_id = 1;
             Brand.save(self.obj, function() {
                 self.brands = Brand.query();
                 self.obj.brand.name = null;
+            });
+        }
+
+        self.destroy = function(brand) {
+            brand.$delete(function () {
+                for (i=0; i < self.brands.length; i++) {
+                    if (self.brands[i].id == brand.id) {
+                        self.brands.splice(i, 1);
+                    }
+                }
             });
         }
     }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,6 +1,6 @@
 angular.module('UmbreoneApp.services', [])
     .factory('Brand', ['$resource', function($resource) {
-        return $resource('http://localhost:3000/brands/:id', null, {
+        return $resource('http://localhost:3000/brands/:id', {id: '@id'}, {
             'update': {method: 'PUT'}
         });
     }]);
