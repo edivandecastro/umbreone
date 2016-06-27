6814b770d55ddf07c73abb53f96f9645d85ff36f
Edivan de Castro <edivandecastro@gmail.com>
2016-06-27 20:42:43 -0300

    Implemented update for brand

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,22 +7,64 @@ angular.module('UmbreoneApp.controllers', [])
         var self = this;
 
         self.brands = Brand.query();
+        $scope.brand = new Brand();
 
         self.save = function() {
-            self.obj.brand.enterprise_id = 1;
-            Brand.save(self.obj, function() {
-                self.brands = Brand.query();
-                self.obj.brand.name = null;
-            });
-        }
+            $scope.brand.enterprise_id = 1;
+            if (brandArePersisted()) {
+                self.update();
+            } else {
+                $scope.brand.$save(function() {
+                    addBrandInBrands();
+                    cleanBrand();
+                });
+            }
+        };
 
         self.destroy = function(brand) {
             brand.$delete(function () {
-                for (i=0; i < self.brands.length; i++) {
-                    if (self.brands[i].id == brand.id) {
-                        self.brands.splice(i, 1);
-                    }
-                }
+                removeBrandInBrands(brand);
             });
-        }
+        };
+
+        self.edit = function(brand) {
+            $scope.brand = new Brand();
+            $scope.brand.id = brand.id;
+            $scope.brand.name = brand.name;
+        };
+
+        self.update = function() {
+            $scope.brand.$update(function() {
+                updateBrandInBrands();
+                cleanBrand();
+            });
+        };
+
+        var cleanBrand = function() {
+            $scope.brand = new Brand();
+        };
+
+        var brandArePersisted = function() {
+            return $scope.brand.id;
+        };
+
+        var addBrandInBrands = function() {
+            self.brands.push($scope.brand);
+        };
+
+        var removeBrandInBrands = function(brand) {
+            self.brands.splice(getBrandIdInBrands(brand), 1);
+        };
+
+        var updateBrandInBrands = function() {
+            self.brands[getBrandIdInBrands($scope.brand)] = $scope.brand;
+        };
+
+        var getBrandIdInBrands = function (brand) {
+            for (var i = 0; i < self.brands.length; i++) {
+                if (self.brands[i].id == brand.id) {
+                    return i;
+                }
+            }
+        };
     }]);
